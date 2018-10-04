f8f6cd836cae235ace4f7d1ba0fde88f41885025
PavanHitesh <pavanfunny@gmail.com>
2018-10-04 22:30:41 +0530

    modified the UI

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -50,4 +50,23 @@ export class SearchComponent implements OnInit {
     console.log("ID",id);
   }
 
+  public filterData(event:any) {
+    //console.log("Data----->",event.target.value);
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
 }
